c1f9991d984df371f6ee76b7dba00293e059be78
vladimirpotekhin <vladimirpotekhin@users.noreply.github.com>
2021-10-21 14:43:29 +0300

    chore: move preview to dialog host

--- projects/demo/src/modules/app/app.browser.module.ts
+++ projects/demo/src/modules/app/app.browser.module.ts
@@ -9,7 +9,6 @@ import {
     TuiThemeAndroidModule,
     TuiThemeIosModule,
 } from '@taiga-ui/addon-mobile';
-import {TuiPreviewHostModule} from '@taiga-ui/addon-preview';
 import {TuiTableBarsHostModule} from '@taiga-ui/addon-tablebars';
 import {TuiDialogModule, TuiLinkModule, TuiModeModule} from '@taiga-ui/core';
 import {MarkdownModule} from 'ngx-markdown';
@@ -38,7 +37,6 @@ import {VersionManagerModule} from './version-manager/version-manager.module';
         TuiMobileDialogModule,
         TuiModeModule,
         TuiLinkModule,
-        TuiPreviewHostModule,
         GettingStartedModule,
         HomeModule,
         LandingModule,
