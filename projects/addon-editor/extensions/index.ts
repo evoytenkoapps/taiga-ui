8d095bbb9d40fca89d277fbce28d16b525b538fa
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2021-10-29 15:33:47 +0300

    feat(addon-editor): resizable images

--- projects/addon-editor/extensions/index.ts
+++ projects/addon-editor/extensions/index.ts
@@ -3,3 +3,5 @@ export * from './background-color';
 export * from './indent-outdent';
 export * from './default-editor-extensions';
 export * from './table-cell-background';
+export * from './tiptap-node-view';
+export * from './image-editor';
